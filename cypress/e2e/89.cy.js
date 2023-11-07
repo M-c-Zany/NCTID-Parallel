b0c0e3c7cea9501f462a8f3ebdab6f664d4d2205
Khaja Dudekela <khaja.dudekela@pixacore.com>
2023-11-07 12:51:55 +0530

    220K to 250K 01

--- cypress/e2e/89.cy.js
+++ cypress/e2e/89.cy.js
@@ -3,12 +3,12 @@ const filePath = "cypress/fixtures/mysql.json";
 let res;
 const filePaths = {
   dataFile1: "mysql.json_1379.json",
-  dataFile2: "mysql.json_1380.json"
+  dataFile2: "mysql.json_1380.json",
 };
 
 const missing_elements = {
   missing1: "cypress/fixtures/missing_elements1379.json",
-  missing2: "cypress/fixtures/missing_elements1380.json"
+  missing2: "cypress/fixtures/missing_elements1380.json",
 };
 
 describe("Fetching data from API and storing it in batch wise and checking elements in all detail pages", () => {
@@ -20,6 +20,7 @@ describe("Fetching data from API and storing it in batch wise and checking eleme
     //Click on Login
     cy.get("#wp-submit").click();
   });
+
 
   it("visit nct ID data", () => {
     const selectors = [
@@ -96,6 +97,10 @@ describe("Fetching data from API and storing it in batch wise and checking eleme
     });
   });
 });
+
+
+
+
 
 describe("Fetching data from API and storing it in batch wise and checking elements in all detail pages", () => {
   beforeEach(() => {
